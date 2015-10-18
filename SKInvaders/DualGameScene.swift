4ccedbc7568429ae2600b21898eb9f5261967ba0
Taroka <kiy535353@hotmail.com>
2015-10-18 12:00:53 -0700

    單人能玩   雙人能連線還不能玩

--- SKInvaders/DualGameScene.swift
+++ SKInvaders/DualGameScene.swift
@@ -12,21 +12,7 @@ import CoreMotion
 
 
 
-extension SKAction {
-    class func shake(duration:CGFloat, amplitudeX:Int = 3, amplitudeY:Int = 3) -> SKAction {
-        let numberOfShakes = duration / 0.015 / 2.0
-        var actionsArray:[SKAction] = []
-        for _ in 1...Int(numberOfShakes) {
-            let dx = CGFloat(arc4random_uniform(UInt32(amplitudeX))) - CGFloat(amplitudeX / 2)
-            let dy = CGFloat(arc4random_uniform(UInt32(amplitudeY))) - CGFloat(amplitudeY / 2)
-            let forward = SKAction.moveByX(dx, y:dy, duration: 0.015)
-            let reverse = forward.reversedAction()
-            actionsArray.append(forward)
-            actionsArray.append(reverse)
-        }
-        return SKAction.sequence(actionsArray)
-    }
-}
+
 
 class DualGameScene: SKScene, SKPhysicsContactDelegate {
     
---
+++ Shake.swift
@@ -0,0 +1,10 @@
+//
+//  Shake.swift
+//  SKInvaders
+//
+//  Created by Sunny Chiu on 10/18/15.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import Foundation
+
